e1b8b5dbfeb85c9b5e392ca70485ffc19944ed4f
KubotaAtsushi <zeptoolmain@gmail.com>
2021-07-13 16:41:33 +0900

    Portfolio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-/* global $*/
+/*global $*/
+$(function(){
+    const moji = $('.animation').text();
+    $('.animation').text('');
+    let count = 0;
+    const text_animation =() =>{
+        let word =moji.substr(count, 1);
+        console.log(word);
+        let span = $('<span>', {text: word});
+        $('.animation').append(span);
+        count++;
+        if(count === moji.length){
+            clearInterval(timer);
+        }
+    }
+    
+    const timer = setInterval(text_animation, 500);
+})
